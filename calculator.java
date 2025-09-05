aeb625a2f4d80510411e29d59ec2d1823c0b4211
E-dakor <edinamkumordjie@gmail.com>
2025-09-05 02:33:35 -1200

    Delete calculator.java

--- calculator.java
+++
@@ -1,4 +0,0 @@
-// In Java, classes are blueprints for creating objects, which are instances of classes. A class defines attributes (data) and methods (functions) that describe the properties and behaviors of the objects. 
-//Classes and objects are crucial in object-oriented programming (OOP) because they promote modularity, encapsulation, and reusability. This allows developers to create more organized and manageable code, reflecting real-world entities.
-//Constructors are special methods used to initialize objects when they are created. They set the initial values of attributes. Methods are functions defined within a class that perform actions or operations on the object's data. Attributes, also known as fields or properties, store the state or characteristics of an object.
- //Together, constructors, methods, and attributes form the foundation of how classes operate in Java, enabling effective data management and functionality.*/
